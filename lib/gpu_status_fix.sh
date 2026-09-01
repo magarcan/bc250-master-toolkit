71d4ce806ca913a56f88316c2cedf2a8f6bffc02
magarcan <manugarrote@gmail.com>
2026-09-01 16:01:05 +0200

    chore: remove obsolete UI compatibility shims

--- lib/gpu_status_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Compatibility shim. The authoritative GPU OC status implementation lives in gpu_oc.sh.
-# Do not read the protected Cyan-Skillfish config here: read-only status must work as a normal user.
-bc250_gpu_oc_status_readonly() {
-  bc250_gpu_oc_status "$@"
-}
